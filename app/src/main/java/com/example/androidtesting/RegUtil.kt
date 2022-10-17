6897ff818122202297bcb1988fad56263491aac1
WillGitAcct <willjhub@outlook.com>
2022-10-17 10:34:25 -0400

    refactor

--- app/src/main/java/com/example/androidtesting/RegUtil.kt
+++ app/src/main/java/com/example/androidtesting/RegUtil.kt
@@ -1,4 +1,27 @@
 package com.example.androidtesting
 
 object RegUtil {
+
+    private val users = listOf("Tim", "Carla", "Reginald")
+    // not valid i the user/pass is empty, or username taken, or pws dont match
+
+    fun validateRegInput(
+        username: String,
+        password: String,
+        confirmedPW: String
+    ): Boolean{
+
+        if(username.isEmpty() || password.isEmpty()){
+            return false
+        }
+
+        if(username in users){
+            return false
+        }
+
+        if(password != confirmedPW){
+            return false
+        }
+        return true
+    }
 }
--- app/src/test/java/com/example/androidtesting/RegUtilTest.kt
+++ app/src/test/java/com/example/androidtesting/RegUtilTest.kt
@@ -1,5 +1,65 @@
 package com.example.androidtesting
 
 import org.junit.Assert.*
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
 
-class RegUtilTest
+class RegUtilTest{
+
+    @Test
+    fun `empty username returns false`(){
+        val result = RegUtil.validateRegInput(
+            username ="",
+            password= "123",
+            confirmedPW = "123"
+        )
+        assertThat(result).isFalse()
+
+    }
+
+    @Test
+    fun `valid username and correct passwords returns true`(){
+        val result = RegUtil.validateRegInput(
+            username ="Timo",
+            password= "1234",
+            confirmedPW = "1234"
+        )
+        assertThat(result).isTrue()
+
+    }
+
+    //should fail
+    @Test
+    fun `username exists and returns false`(){
+        val result = RegUtil.validateRegInput(
+            username ="Tim",
+            password= "123",
+            confirmedPW = "123"
+        )
+        assertThat(result).isFalse()
+
+    }
+
+    @Test
+    fun `username but password empty returns false`(){
+        val result = RegUtil.validateRegInput(
+            username ="Carla",
+            password= "",
+            confirmedPW = ""
+        )
+        assertThat(result).isFalse()
+
+    }
+
+    @Test
+    fun `username but passwords dont match returns false`(){
+        val result = RegUtil.validateRegInput(
+            username ="Carla",
+            password= "123",
+            confirmedPW = "122"
+        )
+        assertThat(result).isFalse()
+
+    }
+
+}
